23eb192657c071cc1726e9c3232a45fac37adbfc
JakobKvejborgUcn <10513107@ucn.dk>
2024-02-22 12:48:21 +0100

    adds a bit more tutorial

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 // lav ny app: 
-// 1. lav ny folder
-// 2. cmd: expo init AwesomeAppName
-// 3. cd awesomeappname
-// 4. code .  (starter appen i vs code studio)
+// number 1 kinda doesn't work. create folder manually
+// 0. open terminal eller kosol. cmd eller ctrl+æ. cd stiTilDetStedDuVilHaveAppenTilAtLigge
+// 1. lav ny folder -> cmd: expo init AwesomeAppName (kan også gøres fra terminalen, ctrl + æ) ((if error, npm install -g expo-cli))
+// 2. cd awesomeappname
+// 3. code .  (starter appen i vs code studio)
 //
 // ctrl + shift + y console
 // npm start for at starte programmet
 // w web
 // ctrl + c for at lukke lortet
 // ()=>console.log("printThisWhenPressed") is method
+
+// How to clone repository from GitHub:
+// 1. Create a folder
+// 2. In terminal: (ctrl + æ) cd path/to/your/directory
+// 3. In terminal: git clone ""insert github clone repository URL here""
+// 4. In terminal: code repository
+// 5. File -> Open folder (ctrl + k)
 
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
